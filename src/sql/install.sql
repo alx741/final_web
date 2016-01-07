11c83fe7b04a592cf260cea1e40c04950fc58db6
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-01-07 18:58:47 -0500

    Usar nuevo usuario SQL

--- src/sql/install.sql
+++ src/sql/install.sql
@@ -1,7 +1,5 @@
-CREATE USER `final`;
-
-CREATE OR REPLACE DATABASE `final`;
-USE `final`;
+CREATE OR REPLACE DATABASE `paqueteria`;
+USE `paqueteria`;
 
 CREATE OR REPLACE TABLE `empleados`
 (
@@ -9,3 +7,7 @@ CREATE OR REPLACE TABLE `empleados`
     `cedula`       varchar(10) NOT NULL,
     `nombre`      varchar(50) NOT NULL
 ) ENGINE = InnoDB;
+
+
+CREATE OR REPLACE USER 'usuario'@'localhost' identified by '1234';
+grant all privileges on paqueteria.* to 'usuario'@'localhost';
