962d9ebec483ca532e5da77cabad843408f98f0d
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-01-08 19:01:37 -0500

    Arreglar instalador SQL

--- src/sql/install.sql
+++ src/sql/install.sql
@@ -12,10 +12,10 @@ CREATE OR REPLACE TABLE `clientes`
 (
     `id_cliente`           integer,
     `nombre_empresa`       varchar(10) NOT NULL,
-    `cedula_representante` varchar(50) NOT NULL
-    `nombre_representante` varchar(50) NOT NULL
-    `telefono`             varchar(50) NOT NULL
-    `direccion`            varchar(50) NOT NULL
+    `cedula_representante` varchar(50) NOT NULL,
+    `nombre_representante` varchar(50) NOT NULL,
+    `telefono`             varchar(50) NOT NULL,
+    `direccion`            varchar(50) NOT NULL,
     `password`             varchar(50) NOT NULL
 ) ENGINE = InnoDB;
 
