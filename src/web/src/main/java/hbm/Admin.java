15ec825ca3b553a7526799fc85053db74c7ed1d2
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-01-18 14:42:35 -0500

    Agregar clase admin

---
+++ src/web/src/main/java/hbm/Admin.java
@@ -0,0 +1,20 @@
+package hbm;
+
+public class Admin
+{
+    private int id;
+    private String password;
+
+    public Admin() {}
+
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+}
